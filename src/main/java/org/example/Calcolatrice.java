0921a20615428adec5c60e0e62d6558b3dbe4b1c
AngeloAntonioPrisco <a.prisco50@studenti.unisa.it>
2023-09-27 22:17:26 +0200

    Aggiunte le funzionalità di somma e di moltiplicazione alla calcolatrice

---
+++ src/main/java/org/example/Calcolatrice.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Calcolatrice {
+    public Calcolatrice() {
+    }
+
+    public int somma(int a, int b){
+        return a+b;
+    }
+
+    public int moltiplicazione(int a, int b){
+        return a*b;
+    }
+}
---
+++ src/test/java/CalcolatriceTest.java
@@ -0,0 +1,23 @@
+import org.example.Calcolatrice;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CalcolatriceTest {
+    Calcolatrice calcolatrice;
+
+    @Before
+    public final void setUp(){
+        calcolatrice = new Calcolatrice();
+    }
+
+    @Test
+    public void testSomma(){
+        assertTrue("SOMMA_CORRETTA",calcolatrice.somma(11,1) == 12);
+    }
+
+    public void testMoltiplicazione(){
+        assertTrue("SOMMA_CORRETTA",calcolatrice.moltiplicazione(10,2) == 20);
+    }
+}
